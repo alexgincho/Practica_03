d1bb6351803c4a360ed8a64169b3f1a25a0878aa
Alexander Chancayauri. Gincho <ginchoalexch@gmail.com>
2021-10-12 19:19:09 -0500

    agregando sweetalert, ajax , creando la vista de agregar producto

--- Practica_03/Controllers/ProductoController.cs
+++ Practica_03/Controllers/ProductoController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Practica_03.Models.Interfaces;
+using Practica_03.Models.ModelsBD;
+using Practica_03.Models.ResponseData;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,50 @@ namespace Practica_03.Controllers
 {
     public class ProductoController : Controller
     {
+        public IProductoService _sP;
+        public ProductoController(IProductoService prod)
+        {
+            this._sP = prod;
+        }
         public IActionResult Index()
         {
+            ViewBag.Productos = _sP.GetAll();
             return View();
+        }
+        public IActionResult Create()
+        {
+            using (var db = new BD_Practica03Context())
+            {
+                ViewBag.Categorias = db.Categoria.ToList();
+            }
+            return View();
+        }
+        // creacion de producto
+
+        [HttpPost]
+        public IActionResult CreateProducto([FromBody] Producto producto)
+        {
+            Response rpta = new Response();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    rpta.Data = _sP.Create(producto);
+                    rpta.State = 200;
+                    rpta.Message = "Exito";
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                rpta.Data = null;
+                rpta.State = 400;
+                rpta.Message = "Error";
+            }
+            return Ok(rpta);
         }
     }
 }
